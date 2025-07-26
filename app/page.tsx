1e8e6b8da3aa4f03e4ee534dc31f2bb65c98bf62
chapter <chuongninolxag@gmail.com>
2025-07-26 16:07:26 +0700

    feat: building the home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,28 @@
+import Image from "next/image";
+import bg from "@/public/bg.png";
+
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      the-wild-oasis-website
-    </div>
+    <main className="mt-24">
+      <Image
+        src={bg}
+        fill
+        alt="Mountains and forests with two cabins"
+        placeholder="blur"
+        className="object-cover object-top"
+        quality={80}
+      />
+      <div className="relative z-10 text-center">
+        <h1 className="text-8xl text-primary-50 mb-10 tracking-tight font-normal">
+          Welcome to paradise.
+        </h1>
+        <a
+          href="/cabins"
+          className="bg-accent-500 px-8 py-6 text-primary-800 text-lg font-semibold hover:bg-accent-600 transition-all"
+        >
+          Explore luxury cabins
+        </a>
+      </div>
+    </main>
   );
 }
