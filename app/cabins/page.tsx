1fea45cf9a0011ec4ec0961aa4ac73a5b8243fe2
chapter <chuongninolxag@gmail.com>
2025-07-26 16:49:44 +0700

    feat: add Cabins page

--- app/cabins/page.tsx
+++ app/cabins/page.tsx
@@ -1,26 +1,35 @@
-import Counter, { UserType } from "@/app/_components/Counter";
 import { Metadata } from "next";
+import CabinCard, { CabinType } from "@/app/_components/CabinCard";
 
 export const metadata: Metadata = {
   title: "Cabins",
 };
 
 export default async function Page() {
-  const res = await fetch("https://jsonplaceholder.typicode.com/users");
-  const data: UserType[] = await res.json();
-  console.log(data);
+  // CHANGE
+  const cabins: CabinType[] = [];
 
   return (
     <div>
-      <h1>Cabins page</h1>
-      <ul>
-        {data.map((user: UserType) => (
-          <li key={user.id}>
-            {user.name} - {user.email}
-          </li>
-        ))}
-      </ul>
-      <Counter users={data} />
+      <h1 className="text-4xl mb-5 text-accent-400 font-medium">
+        Our Luxury Cabins
+      </h1>
+      <p className="text-primary-200 text-lg mb-10">
+        Cozy yet luxurious cabins, located right in the heart of the Italian
+        Dolomites. Imagine waking up to beautiful mountain views, spending your
+        days exploring the dark forests around, or just relaxing in your private
+        hot tub under the stars. Enjoy nature&#39;s beauty in your own little
+        home away from home. The perfect spot for a peaceful, calm vacation.
+        Welcome to paradise.
+      </p>
+
+      {cabins.length > 0 && (
+        <div className="grid sm:grid-cols-1 md:grid-cols-2 gap-8 lg:gap-12 xl:gap-14">
+          {cabins.map((cabin) => (
+            <CabinCard cabin={cabin} key={cabin.id} />
+          ))}
+        </div>
+      )}
     </div>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import bg from "@/public/bg.png";
+import Link from "next/link";
 
 export default function Home() {
   return (
@@ -16,12 +17,12 @@ export default function Home() {
         <h1 className="text-8xl text-primary-50 mb-10 tracking-tight font-normal">
           Welcome to paradise.
         </h1>
-        <a
+        <Link
           href="/cabins"
           className="bg-accent-500 px-8 py-6 text-primary-800 text-lg font-semibold hover:bg-accent-600 transition-all"
         >
           Explore luxury cabins
-        </a>
+        </Link>
       </div>
     </main>
   );
