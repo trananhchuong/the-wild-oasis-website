4f7d1add9185a65602ff885c09ba36018dddba22
chapter <chuongninolxag@gmail.com>
2025-08-24 22:35:36 +0700

    feat: CabinList add Filter compomemt

--- app/cabins/page.tsx
+++ app/cabins/page.tsx
@@ -2,6 +2,7 @@ import { Metadata } from "next";
 import { Suspense } from "react";
 import CabinList from "@/app/_components/CabinList";
 import Spinner from "@/app/_components/Spinner";
+import Filter from "../_components/Filter";
 
 export const revalidate = 3600; // 1 hour in seconds
 
@@ -9,7 +10,13 @@ export const metadata: Metadata = {
   title: "Cabins",
 };
 
-export default function Page() {
+type PageProps = {
+  searchParams: { capacity?: string };
+};
+
+export default function Page({ searchParams }: PageProps) {
+  const filter = searchParams.capacity ?? "all";
+
   return (
     <div>
       <h1 className="text-4xl mb-5 text-accent-400 font-medium">
@@ -23,8 +30,12 @@ export default function Page() {
         home away from home. The perfect spot for a peaceful, calm vacation.
         Welcome to paradise.
       </p>
-      <Suspense fallback={<Spinner />}>
-        <CabinList />
+
+      <div className="flex justify-end mb-8">
+        <Filter />
+      </div>
+      <Suspense fallback={<Spinner />} key={filter}>
+        <CabinList filter={filter as string} />
       </Suspense>
     </div>
   );
